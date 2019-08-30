30cd5145888d08193e910056e5545f40cf530915
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2019-08-30 23:57:35 +0300

    set color scheme

--- conf.py
+++ conf.py
@@ -873,7 +873,7 @@ IMAGE_FOLDERS = {'images': 'images'}
 # paraiso-light, pastie, perldoc, rrt, tango, trac, vim, vs, xcode
 # This list MAY be incomplete since pygments adds styles every now and then.
 # Check with list(pygments.styles.get_all_styles()) in an interpreter.
-# CODE_COLOR_SCHEME = 'default'
+CODE_COLOR_SCHEME = 'tango'
 
 # FAVICONS contains (name, file, size) tuples.
 # Used to create favicon link like this:
